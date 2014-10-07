2ceed86b5ed6d4c118390187dcf80b5467696c07
Jiahe Kuang <Jiahekuang@Jiahes-MacBook-Pro.local>
2014-10-07 16:20:36 -0700

    fixed UI transition and have audio recording working

--- StampNotes/RecordingScreenViewController.m
+++ StampNotes/RecordingScreenViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "RecordingScreenViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface RecordingScreenViewController ()
+
+@interface RecordingScreenViewController () <AVAudioRecorderDelegate, AVAudioPlayerDelegate>
+
+@property(strong, nonatomic) AVAudioRecorder *recorder;
+@property(strong, nonatomic) AVAudioPlayer *player;
+@property (weak, nonatomic) IBOutlet UIButton *startRecordingButton;
+@property (weak, nonatomic) IBOutlet UIButton *finishedRecordingButton;
+@property (weak, nonatomic) IBOutlet UIButton *playRecordingButton;
+
 
 @end
 
@@ -16,12 +25,111 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self.finishedRecordingButton setEnabled:NO];
+    [self.playRecordingButton setEnabled:NO];
+    
+    // Set the audio file
+    NSArray *pathComponents = [NSArray arrayWithObjects:
+                               [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
+                               @"demo.m4a",
+                               nil];
+    
+    
+    NSURL *outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
+    
+    // Setup audio session
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    
+    // Define the recorder setting
+    NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc] init];
+    
+    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
+    [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+    [recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
+    
+    // Initiate and prepare the recorder
+    self.recorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:NULL];
+    self.recorder.delegate = self;
+    self.recorder.meteringEnabled = YES;
+    [self.recorder prepareToRecord];
+    
     // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)recordingPlay:(id)sender
+{
+    if (!self.recorder.recording){
+        self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:self.recorder.url error:nil];
+        [self.player setDelegate:self];
+        [self.player play];
+    }
+
+}
+
+- (IBAction)recordingStartOrPause:(id)sender
+{
+    
+    //When a recording is being played, it will be paused
+    if (self.player.playing)
+    {
+        [self.player stop];
+    }
+    
+    //When it is not recording, it will start a new recording
+    if (!self.recorder.recording) {
+        AVAudioSession *session = [AVAudioSession sharedInstance];
+        
+        [session setActive:YES error:nil];
+        
+        //start recroding
+        [self.recorder record];
+        [self.startRecordingButton setTitle:@"Pause" forState:UIControlStateNormal];
+    }
+    
+    //when it is recording, we want to pause
+    else
+    {
+        [self.recorder pause];
+        [self.startRecordingButton setTitle:@"Start" forState:UIControlStateNormal];
+    }
+    
+    [self.finishedRecordingButton setEnabled:YES];
+    [self.playRecordingButton setEnabled:NO];
+    
+    
+}
+
+- (IBAction)recordingFinished:(id)sender
+{
+    [self.recorder stop];
+    
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setActive:NO error:nil];
+//    [self.navigationController popViewControllerAnimated:YES];
+
+}
+
+- (void) audioRecorderDidFinishRecording:(AVAudioRecorder *)avrecorder successfully:(BOOL)flag{
+    [self.startRecordingButton setTitle:@"Start" forState:UIControlStateNormal];
+    
+    [self.finishedRecordingButton setEnabled:NO];
+    [self.playRecordingButton setEnabled:YES];
+}
+
+- (void) audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Done"
+                                                    message: @"Finish playing the recording!"
+                                                   delegate: nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
 }
 
 /*
--- StampNotes/SNMainTableViewController.m
+++ StampNotes/SNMainTableViewController.m
@@ -8,6 +8,7 @@
 
 #import "SNMainTableViewController.h"
 #import "Recording.h"
+#import "RecordingScreenViewController.h"
 #import "AppDelegate.h"
 
 
@@ -77,82 +78,47 @@
     
     return cell;
 }
-- (IBAction)createNewClass:(id)sender
-{
-    UIAlertView * alert =[[UIAlertView alloc ] initWithTitle:@"Enter a Title" message:@"" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Create", nil];
-    alert.alertViewStyle = UIAlertViewStylePlainTextInput;
-    [alert show];
-}
 
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
-{
 
-    if (buttonIndex ==1)
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"newRecordingSession"])
     {
         NSManagedObjectContext *recordingContext = ((AppDelegate *)[UIApplication sharedApplication].delegate).managedObjectContext;
         
         Recording *newRecording = [NSEntityDescription insertNewObjectForEntityForName:@"Recording" inManagedObjectContext:recordingContext];
-        newRecording.name = [alertView textFieldAtIndex:0].text;
+        
+        newRecording.date = [NSDate date];
+        
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        
+        [dateFormatter setDateFormat:@"yyyy-MM-dd hh:mm:ss a"];
+        
+        newRecording.name = [NSString stringWithFormat:@"Note(s) %@", [dateFormatter stringFromDate:newRecording.date]];
+        
         [self.recordings addObject:newRecording];
         [(AppDelegate *)[UIApplication sharedApplication].delegate saveContext];
-        [self performSegueWithIdentifier: @"RecordingScreen" sender: self];
 
     }
 }
 
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
-}
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+    
+    NSManagedObjectContext *candyContext = ((AppDelegate *)[UIApplication sharedApplication].delegate).managedObjectContext;
+    
+    
+    [candyContext deleteObject: self.recordings[indexPath.row]];
+    
+    [(AppDelegate *)[UIApplication sharedApplication].delegate saveContext];
+    
+    
+    [self.recordings removeObjectAtIndex:indexPath.row];
+    
+    [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
